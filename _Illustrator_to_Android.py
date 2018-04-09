79f3c76cc8fcfd081c64db8d19957eb58507bb88
roman-ku <roman-ku@users.noreply.github.com>
2018-04-08 21:47:00 -0700

    Simplify code; Add an easy way to change the prefix; Don't make unnecessary folders

--- _Illustrator_to_Android.py
+++ _Illustrator_to_Android.py
@@ -1,62 +1,55 @@
-#Android file sorter for illustrator exports
-#By: Jorge Bautista <jorgebautista@gmail.com>
-#This utility allows you to arrange the exported resources from illustrator for android 
-#application development. The script removes the extra information on the file names and
-#put each file on the correct folder.
-#
-#
 
+# Original script by:
+# Android file sorter for illustrator exports
+# By: Jorge Bautista <jorgebautista@gmail.com>
+# This utility allows you to arrange the exported resources from illustrator for android 
+# application development. The script removes the extra information on the file names and
+# put each file on the correct folder.
+
+# Updated version by:
+# Roman Kuleshov
+# March 8, 2018
+#   CHANGES
+# + simplified code
+# + add easy way to change the prefix
+# + don't make unnecessary folders
 
 import glob, re, os
 from shutil import move,copy2
 
-#Directories to be created
-folders=['mipmap-xxxhdpi','mipmap-xxhdpi','mipmap-xhdpi','mipmap-hdpi','mipmap-mdpi','mipmap-ldpi']
+folderprefix = 'drawable-'
 
-#absoluthe path to our location
-main_folder=os.path.dirname(__file__)
-
-#loop to create directories in case they don't exists
-for folder in folders:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-#array with the extensions needed to clasify the different files
-extensions=['xxxhdpi.png','xxhdpi.png','xhdpi.png','hdpi.png','mdpi.png','ldpi.png']    
+# array with the sizes needed to clasify the different files
+sizes = ['xxxhdpi','xxhdpi','xhdpi','hdpi','mdpi','ldpi']    
 
 #loop to check all the files in the folder
 for filename in glob.glob('*.png'):
 
-
-#loop through the different extensions and clasify the file
-    for x in range(0, 6):
+	# loop through the different sizes and clasify the file
+    for size in sizes:
         
-        #Check if the file name contains that extension        
-        if extensions[x] in filename:
+        # Check if the file name contains that extension        
+        if size in filename:
             
-           #dst will have our destination folder hdpi, xhdpi etc.
-            dst=folders[x]+'/'
+            # dst will have our destination folder hdpi, xhdpi etc.
+            foldername = folderprefix + size
 
-            #new name for our file (without the xxxhdpi, xxhdpi, etc)
-            new_name=(filename).replace(extensions[x],".png",1)
+            # new name for our file (without the xxxhdpi, xxhdpi, etc)
+            filename_new = filename.replace(size, '')
+
+            # create folder (if we need to)
+            if not os.path.exists(foldername):
+                os.makedirs(foldername)
 
             # if the destination file exists, it will be deleted
-            if os.path.exists(main_folder+'/'+dst+filename):                
-                os.remove(dst+filename)
-            if os.path.exists(main_folder+'/'+dst+new_name):                
-                os.remove(dst+new_name)
-                print ('removed '+dst+new_name)
-            print(main_folder+'/'+dst+new_name)
-            
-            #move the new file to the destination folder
-            move(filename,dst,copy_function=copy2)
-            print ('moving '+dst+new_name)
-            
-            #leave the file with the appropriate name (without the xxxhdpi, xxhdpi, etc)                           
-            os.rename(dst+filename, dst+ new_name)
+            if os.path.exists(os.path.join(foldername, filename_new)):                
+                os.remove(os.path.join(foldername, filename_new))
+                print ('Deleting stale copy of "{}"'.format(os.path.join(foldername, filename_new)))
 
-            #we exit to continue with the next file
+            print('Moving "{}" to "{}"'.format(filename, foldername))
+            
+            # move the new file to the destination folder
+            move(filename, os.path.join(foldername, filename_new) ,copy_function=copy2)
+            
+            # we exit to continue with the next file
             break
-                
-      
-      
